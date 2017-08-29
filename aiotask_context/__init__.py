6b86b1d64bdb4f6d24c0b927de8a0ddf76994e41
Timur Izhbulatov <timochka@gmail.com>
2017-08-29 20:28:19 +0300

    Loop already takes care of raising RuntimeError

--- aiotask_context/__init__.py
+++ aiotask_context/__init__.py
@@ -6,10 +6,6 @@ logger = logging.getLogger(__name__)
 
 
 def task_factory(loop, coro):
-    # For uvloop loop._check_closed is private
-    if loop.is_closed():
-        raise RuntimeError('Event loop is closed')
-
     task = asyncio.tasks.Task(coro, loop=loop)
     if task._source_traceback:
         del task._source_traceback[-1]
--- tests/test_context.py
+++ tests/test_context.py
@@ -54,3 +54,9 @@ class TestContext:
         assert event_loop.run_until_complete(coro(event_loop)) is True
         asyncio.set_event_loop(None)
         assert event_loop.run_until_complete(coro(event_loop)) is True
+
+
+    def test_closed_loop(self, event_loop):
+        event_loop.close()
+        with pytest.raises(RuntimeError):
+            context.task_factory(event_loop, dummy())
