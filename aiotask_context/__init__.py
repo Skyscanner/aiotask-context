7d14c7d83844a38f99713633ffa919d04e1e9e11
Alex Burbidge <alex@expend.io>
2017-10-24 09:23:30 +0100

    Add copying_task_factory (#13)
    The copying_task_factory does not share the context
    when creating new tasks and uses a copy of it.
    This is to allow created tasks to modify their context
    without mirroring the change into every other task, but
    whilst still passing along a copy of their own context
    to their children.

--- aiotask_context/__init__.py
+++ aiotask_context/__init__.py
@@ -1,24 +1,40 @@
 import asyncio
 import logging
+from copy import deepcopy
 
 
 logger = logging.getLogger(__name__)
 
-
 NO_LOOP_EXCEPTION_MSG = "No event loop found, key {} couldn't be set"
 
 
-def task_factory(loop, coro):
+def task_factory(loop, coro, copy_context=False):
     task = asyncio.tasks.Task(coro, loop=loop)
     if task._source_traceback:
         del task._source_traceback[-1]
 
     try:
-        task.context = asyncio.Task.current_task(loop=loop).context
+        context = asyncio.Task.current_task(loop=loop).context
+        if copy_context:
+            context = deepcopy(context)
+
+        task.context = context
     except AttributeError:
         task.context = {}
 
     return task
+
+
+def copying_task_factory(loop, coro):
+    """
+    Returns a task factory that copies a task's context into new tasks instead of
+    sharing it.
+
+    :param loop: The active event loop
+    :param coro: A coroutine object
+    :return: A context copying task factory
+    """
+    return task_factory(loop, coro, copy_context=True)
 
 
 def get(key, default=None):
